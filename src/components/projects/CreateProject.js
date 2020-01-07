4eb66c0d599a1bd4be9c9fdc35cf354a138f607c
Manvitha Konkimalla <manvitha041@gmail.com>
2020-01-07 21:13:12 +0100

    updated new content

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,60 +1,95 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { createProject } from '../../store/actions/projectActions'
-import { Redirect } from 'react-router-dom'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { createProject } from "../../store/actions/projectActions";
+import { Redirect } from "react-router-dom";
 
 
 class CreateProject extends Component {
   state = {
-    title: '',
-    content: ''
-  }
-  handleChange = (e) => {
+    title: "",
+    content: "",
+    file: '',
+    imagePreviewUrl: ''
+  };
+  handleChange = e => {
     this.setState({
       [e.target.id]: e.target.value
-    })
-  }
-  handleSubmit = (e) => {
+    });
+  };
+  handleSubmit = e => {
     e.preventDefault();
     // console.log(this.state);
     this.props.createProject(this.state);
-    this.props.history.push('/');
-  }
+    this.props.history.push("/");
+  };
+  handleImageChange = e => {
+    
+
+    let reader = new FileReader();
+    let file = e.target.files[0];
+
+    reader.onloadend = () => {
+      this.setState({
+        file: file,
+        imagePreviewUrl: reader.result
+      });
+    };
+
+    reader.readAsDataURL(file);
+  };
+  fileUploadHandler = e => {
+ 
+    console.log("handle uploading-", this.state.file);
+  };
   render() {
+    let { imagePreviewUrl } = this.state;
+    let $imagePreview = null;
+    if (imagePreviewUrl) {
+      $imagePreview = <img src={imagePreviewUrl} />;
+    } 
+
     const { auth } = this.props;
-    if(!auth.uid) return <Redirect to ='/signIn' />
+    if (!auth.uid) return <Redirect to="/signIn" />;
     return (
       <div className="container">
         <form className="white" onSubmit={this.handleSubmit}>
-          <h5 className="grey-text text-darken-3">Create a New Project</h5>
+          <h5 className="grey-text text-darken-3">Upload a New Photo</h5>
           <div className="input-field">
-            <input type="text" id='title' onChange={this.handleChange} />
-            <label htmlFor="title">Project Title</label>
+            <input type="text" id="title" onChange={this.handleChange} />
+            <label htmlFor="title">Name</label>
           </div>
           <div className="input-field">
-            <textarea id="content" className="materialize-textarea" onChange={this.handleChange}></textarea>
-            <label htmlFor="content">Project Content</label>
+            <textarea
+              id="content"
+              className="materialize-textarea"
+              onChange={this.handleChange}
+            ></textarea>
+            <label htmlFor="content">Description</label>
+          </div>
+          <div className="previewComponent">
+            <input type="file" name="file" onChange={this.handleImageChange} />
+            <button onClick={this.fileUploadHandler}>Upload</button>
+            <div className="imgPreview">{$imagePreview}</div>
           </div>
           <div className="input-field">
             <button className="btn pink lighten-1">Create</button>
           </div>
         </form>
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps =(state) =>{
-  return{
+const mapStateToProps = state => {
+  return {
     auth: state.firebase.auth
-  
-  }
-  }
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
-    createProject: (project) => dispatch(createProject(project))
-  }
-}
+    createProject: project => dispatch(createProject(project))
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(CreateProject)
+export default connect(mapStateToProps, mapDispatchToProps)(CreateProject);
